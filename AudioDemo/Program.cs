63277bbcb6c2427a57ea86d42a764f4a51e97e19
Opilane TTHK <opilane@tthk.ee>
2021-11-10 10:43:31 +0200

    AudioDemo

---
+++ AudioDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using static System.Console;
+
+namespace AudioDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.White;
+            WriteLine(@"
+                                              ,           ^'^  _
+                                              )               (_) ^'^
+         _/\_                    .---------. ((        ^'^
+         (('>                    )`'`'`'`'`( ||                 ^'^
+    _    /^|                    /`'`'`'`'`'`\||           ^'^
+    =>--/__|m---               /`'`'`'`'`'`'`\|
+         ^^           ,,,,,,, /`'`'`'`'`'`'`'`\      ,
+                     .-------.`|`````````````|`  .   )
+                    / .^. .^. \|  ,^^, ,^^,  |  / \ ((
+                   /  |_| |_|  \  |__| |__|  | /,-,\||
+        _         /_____________\ |')| |  |  |/ |_| \|
+       (')         |  __   __  |  '==' '=='  /_______\     _
+      (' ')        | /  \ /  \ |   _______   |,^, ,^,|    (')
+       \  \        | |--| |--| |  ((--.--))  ||_| |_||   (' ')
+     _  ^^^ _      | |__| |('| |  ||  |  ||  |,-, ,-,|   /  /
+   ,' ',  ,' ',    |           |  ||  |  ||  ||_| |_||   ^^^
+.,,|RIP|,.|RIP|,.,,'==========='==''=='==''=='=======',,....,,,,.,ldb
+== Credits ==
+Music: 'Fun House' by Coyote Hearing from youtube.com/audiolibrary
+Art: Haunted house by ldb from https://www.asciiart.eu/holiday-and-events/halloween
+");
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine("\n(Press any key to exit)");
+            ReadKey();
+        }
+    }
+}
